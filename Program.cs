d65caaf14c5f93959be690f207e7e1acb143e02d
NebulaDev <nebuladev.contact@gmail.com>
2021-12-16 10:50:40 +1300

    Version 2.0 push
    The update was quite large, so I renamed it to 2.0. Enjoy!

--- Program.cs
+++ Program.cs
@@ -8,10 +8,40 @@ namespace Constructivity.Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Nebula Constructivity Demo.\n");
+            Console.ReadKey();
 
             while (true) {
-                string output = Construct.NewModernSentence(Question.Int("Sentence Length?"), Question.Bool("Use punctuation?"));
-                // ----> Random Sentence: string output = Construct.NewRandomSentence(Question.Int("Sentence Length?"), Question.Bool("Use characters like z, x, j, etc..? (Uncommon characters)"), Question.Bool("Use punctuation?"));
+                Console.Clear();
+                Console.WriteLine("[0]. Random Sentence");
+                Console.WriteLine("[1]. Modern Sentence");
+                Console.WriteLine("[2]. Random Chars");
+                int.TryParse(Console.ReadLine(), out int a);
+
+                Console.Clear();
+
+                string output = "";
+                switch (a) {
+                    case 0:
+                    // ----> Random Sentence:
+                        output = Construct.NewRandomSentence(Question.Int("Sentence Length?"), Question.Bool("Use characters like z, x, j, etc..? (Uncommon characters)"), Question.Bool("Use punctuation?"));
+                        break;
+
+                    case 1:
+                    // ----> Modern Sentence (beta):
+                        output = Construct.NewModernSentence(Question.Int("Sentence Length?"), Question.Bool("Use punctuation?"));
+                        break;
+
+                    case 2:
+                    // ----> Random Chars:
+                        output = Construct.RandomChars(Question.Int("Sentence Length?"));
+                        break;
+
+                    default:
+                        Console.WriteLine("Please spesify a sentence type.");
+                        continue;
+                }
+                
+                
                 
                 // I'm using a quickly whipped up class for the questions. Find it in Question.cs.
                 Console.Clear();
