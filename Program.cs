32d20701bac0e5f86eaf96673f257ceb6f5e6c98
NebulaDev <nebuladev.contact@gmail.com>
2021-12-16 08:43:45 +1300

    Started Constructivity migration from pc
    Pushed first build, (made overnight when I was really tired..)

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Constructivity;
+
+namespace Constructivity.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Nebula Constructivity Demo.\n");
+
+            while (true) {
+                string output = Construct.NewRandomSentence(Question.Int("Sentence Length?"), Question.Bool("Use characters like z, x, j, etc..? (Uncommon characters)"), Question.Bool("Use punctuation?"));
+                // I'm using a quickly whipped up class for the questions. Find it in Question.cs.
+                Console.WriteLine("Result:\n{0}", output);
+                Console.ReadKey(true);
+            }
+
+        }
+    }
+}
